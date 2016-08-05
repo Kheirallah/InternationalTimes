f5426676f89f46c5a70d8cb220bdcb4bf3e4c96e
Kheirallah <kheirallah15@gmail.com>
2016-08-05 13:52:44 -0400

    completed program
    Program is complete, only comments are needed to elaborate

--- internationalTimes.java
+++ internationalTimes.java
@@ -5,66 +5,62 @@ import java.util.TimeZone;
 public class internationalTimes
 {
 
-public static void main(String argv[])
-{
+   public static void main(String argv[])
+   {
 
-Calendar cal = Calendar.getInstance();
+   Calendar cal = Calendar.getInstance();
 
-//Default time zone is America/New_York
-TimeZone time = TimeZone.getDefault();
+   //Default time zone is America/New_York
+   TimeZone time = TimeZone.getDefault();
 
-System.out.println("Time zone:            City:             Time:");
+   System.out.println("Time zone:            City:             Time:");
 
-//Prints out the time and timezone in Washington DC
-SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
+   //Prints out the time and timezone in Washington DC
+   SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
 
-System.out.println();
+   System.out.println();
 
-if (cal.get(Calendar.AM_PM) == 0)
-System.out.println(time.getID() + "      " + "Washington DC     " + sdf.format(cal.getTime()) + " AM");
-else
-System.out.println(time.getID() + "      " + "Washington DC     " + sdf.format(cal.getTime()) + " PM");
+      if (cal.get(Calendar.AM_PM) == 0)
+         System.out.println(time.getID() + "      " + "Washington DC     " + sdf.format(cal.getTime()) + " AM");
+      else
+         System.out.println(time.getID() + "      " + "Washington DC     " + sdf.format(cal.getTime()) + " PM");
 
-//Prints out the time in Los Angeles
-time = TimeZone.getTimeZone("America/Los_Angeles");
-sdf.setTimeZone(time);
+   //Prints out the time in Los Angeles
+   time = TimeZone.getTimeZone("America/Los_Angeles");
+   sdf.setTimeZone(time);
 
-if (cal.get(Calendar.AM_PM) == 0)
-System.out.println(time.getID() + "   " + "Los Angeles       " + sdf.format(cal.getTime()) + " AM");
-else
-System.out.println(time.getID() + "   " + "Los Angeles       " + sdf.format(cal.getTime()) + " PM");
+      if (cal.get(Calendar.AM_PM) == 0)
+         System.out.println(time.getID() + "   " + "Los Angeles       " + sdf.format(cal.getTime()) + " AM");
+      else
+         System.out.println(time.getID() + "   " + "Los Angeles       " + sdf.format(cal.getTime()) + " PM");
 
-//Prints out the time in Amsterdam
-time = TimeZone.getTimeZone("Europe/Amsterdam");
-sdf.setTimeZone(time);
+   //Prints out the time in Amsterdam
+   time = TimeZone.getTimeZone("Europe/Amsterdam");
+   sdf.setTimeZone(time);
 
-if (cal.get(Calendar.AM_PM) == 1)
-System.out.println("The time in Amsterdam is: " + sdf.format(cal.getTime()) + " AM");
-else
-System.out.println("The time in Amsterdam is: " + sdf.format(cal.getTime()) + " PM");
+      if (cal.get(Calendar.AM_PM) == 1)
+         System.out.println(time.getID() + "   " + "   Amsterdam         " + sdf.format(cal.getTime()) + " AM");
+      else
+         System.out.println(time.getID() + "   " + "   Amsterdam         " + sdf.format(cal.getTime()) + " PM");
 
-System.out.println();
+   //Prints out the time in Jerusalem
+   time = TimeZone.getTimeZone("Asia/Jerusalem");
+   sdf.setTimeZone(time);
 
-//Prints out the time in Jerusalem
-time = TimeZone.getTimeZone("Asia/Jerusalem");
-sdf.setTimeZone(time);
+      if (cal.get(Calendar.AM_PM) == 1)
+         System.out.println(time.getID() + "   " + "     Jerusalem         " + sdf.format(cal.getTime()) + " AM");
+      else
+         System.out.println(time.getID() + "   " + "     Jerusalem         " + sdf.format(cal.getTime()) + " PM");
 
-if (cal.get(Calendar.AM_PM) == 1)
-System.out.println("The time in Jerusalem is: " + sdf.format(cal.getTime()) + " AM");
-else
-System.out.println("The time in Jerusalem is: " + sdf.format(cal.getTime()) + " PM");
+   //Prints out the time in Hong Kong
+   time = TimeZone.getTimeZone("Asia/Hong_Kong");
+   sdf.setTimeZone(time);
+   
+      if (cal.get(Calendar.AM_PM) == 1)
+         System.out.println(time.getID() + "   " + "     Hong Kong         " + sdf.format(cal.getTime()) + " AM");
+      else
+         System.out.println(time.getID() + "   " + "     Hong Kong         " + sdf.format(cal.getTime()) + " PM");
 
-System.out.println();
-
-//Prints out the time in Hong Kong
-time = TimeZone.getTimeZone("Asia/Hong_Kong");
-sdf.setTimeZone(time);
-
-if (cal.get(Calendar.AM_PM) == 1)
-System.out.println("The time in Hong Kong is: " + sdf.format(cal.getTime()) + " AM");
-else
-System.out.println("The time in Hong Kong is: " + sdf.format(cal.getTime()) + " PM");
-
-}
+   }
 
 }
